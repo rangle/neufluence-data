b31c5650895a17131f3a1010e008c1ac1d44ae27
Yaxin Wan <yaxinwan@yaxins-mbp.myfiosgateway.com>
2021-03-15 12:17:34 -0400

    update function

--- neufluence_data/instagram/influencer/ig_influencer_dao.py
+++ neufluence_data/instagram/influencer/ig_influencer_dao.py
@@ -6,7 +6,23 @@ class IGInfluencerDAO():
 
     def get_influencer_by_user_name(user_name):
         #define
-        return 0
+        db = firebase.get_firebase_db()
+        print("got db")
+        print("searching for user: " + user_name)
+
+        influencers_ref = db.collection(u'social_media')
+        query = influencers_ref.where(
+                    u'user_name',u'==', user_name).limit(1)
+        results = query.stream()
+        print("results")
+        print(results)
+
+        for result in results:
+            print("found 1 result")
+            print(f'{result.id} => {result.to_dict()}')
+            influencer_ref = db.collection(u'social_media').document(result.id)
+        return influencer_ref
+        # edge case to ahndle in case there's no influencer, throw an exception InfluencerScrapedNotFoundException
 
 
     def get_influencer_api_by_user_name(user_name):
@@ -34,9 +50,7 @@ class IGInfluencerDAO():
             influencer_ref = db.collection(u'influencers_scraped').document(result.id)
 
         # edge case to ahndle in case there's no influencer, throw an exception InfluencerScrapedNotFoundException
-
         return influencer_ref
-
 
     def get_influencer_scraped_by_id(influencer_id):
         db = firebase.get_firebase_db()
@@ -45,7 +59,10 @@ class IGInfluencerDAO():
 
         return influencer
 
-
-    def get_influencer_api_by_user_name(user_name):
-        #define
-        return 0
+    def save_total_legit_followers(user_name,total_legit_followers):
+        influencer_ref = IGInfluencerDAO.get_influencer_by_user_name(user_name)
+        influencer_ref.update({
+        u'total_legit_followers':total_legit_followers
+        })
+        print("save total legit followers")
+        return
--- neufluence_data/test.py
+++ neufluence_data/test.py
@@ -3,15 +3,22 @@ import neufluence_firebase as firebase
 from instagram.influencer.ig_influencer_dao import IGInfluencerDAO
 from instagram.follower.ig_follower_dao import IGFollowerDAO
 from instagram.follower.ig_follower_vo import IGFollowerSimpleVO
-
+from instagram.follower.ig_follower_vo import IGFollowerVO
 
 # create follower list from driver
-f1 = IGFollowerSimpleVO("user_name_1", "user_full_name_1", "url")
-f2 = IGFollowerSimpleVO("user_name_2", "user_full_name_2", "url")
+#f1 = IGFollowerSimpleVO("user_name_1", "user_full_name_1", "url")
+#f2 = IGFollowerSimpleVO("user_name_2", "user_full_name_2", "url")
+
+#f3 = IGFollowerVO("user_name_1", "user_full_name_1", "profile_pic_url_1",200, 100, 50)
+#f4 = IGFollowerVO("user_name_2", "user_full_name_2", "profile_pic_url_2",300, 200, 80)
 
 # save list of followerVO
-influencer_user_name = "bianca_test"
-
-ig_influencer = IGInfluencerDAO.get_influencer_scraped_by_user_name(influencer_user_name)
-
+#influencer_user_name = "yaxin"
+#ig_influencer = IGInfluencerDAO.get_influencer_scraped_by_user_name(influencer_user_name)
 #IGFollowerDAO.save_list_of_followers_simple(influencer_user_name, [f1, f2])
+#IGFollowerDAO.save_list_of_followers(influencer_user_name, [f3, f4])
+
+influencer_user_name = "nadiamariesasso"
+total_legit_followers = 200
+#ig_influencer = IGInfluencerDAO.get_influencer_by_user_name(influencer_user_name)
+IGInfluencerDAO.save_total_legit_followers(influencer_user_name,total_legit_followers)
